08a0ac5cee9f49af56ce66063829e32a23ea1d37
meryemaali <66699659+meryemaali@users.noreply.github.com>
2022-04-18 16:46:45 +0000

    Update validateAccountCustomer.php

--- manager/validateAccountCustomer.php
+++ manager/validateAccountCustomer.php
@@ -41,12 +41,12 @@ if(isset($_POST['validateAccount'])){
         $mail->isSMTP(); // Paramétrer le Mailer pour utiliser SMTP 
         $mail->Host = 'smtp.gmail.com'; // Spécifier le serveur SMTP
         $mail->SMTPAuth = true; // Activer authentication SMTP
-        $mail->Username = 'myturn.projet@gmail.com'; // Votre adresse email d'envoi
-        $mail->Password = 'myturn1234'; // Le mot de passe de cette adresse email
+        $mail->Username = '***************'; // Votre adresse email d'envoi
+        $mail->Password = '*******'; // Le mot de passe de cette adresse email
         $mail->SMTPSecure = "ssl"; // Accepter SSL
         $mail->Port = 465;
 
-        $mail->setFrom('myturn.projet@gmail.com', 'Mailer'); // Personnaliser l'envoyeur
+        $mail->setFrom('*************', 'Mailer'); // Personnaliser l'envoyeur
         $mail->addAddress($email, 'User'); // Ajouter le destinataire
 
         $mail->isHTML(true);
